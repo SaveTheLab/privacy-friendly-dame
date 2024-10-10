593debfd6b6b82b754cfe0f01b93f9ffcb879522
SaveTheLab <savethelab.dev@gmail.com>
2024-10-10 22:22:20 +0200

    Updated target SDK and version
    Updated terget SDK version to 35.
    Updated version to 1.0C

--- app/src/main/java/org/secuso/privacyfriendlydame/PFDameApplication.kt
+++ app/src/main/java/org/secuso/privacyfriendlydame/PFDameApplication.kt
@@ -2,11 +2,21 @@ package org.secuso.privacyfriendlydame
 
 
 import android.app.Application
+import android.util.Log
+import androidx.work.Configuration
+import org.secuso.privacyfriendlybackup.api.pfa.BackupManager
+import org.secuso.privacyfriendlydame.backup.BackupCreator
+import org.secuso.privacyfriendlydame.backup.BackupRestorer
 
-class PFDameApplication : Application() {
+class PFDameApplication : Application(), Configuration.Provider {
 
     override fun onCreate() {
         super.onCreate()
+        BackupManager.backupCreator = BackupCreator()
+        BackupManager.backupRestorer = BackupRestorer()
     }
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder().setMinimumLoggingLevel(Log.INFO).build()
 
 }
